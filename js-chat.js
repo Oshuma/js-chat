b5aec23e2dc5dec67b66004a690f09203348adef
Dale Campbell <dale@save-state.net>
2009-06-28 21:45:30 -0500

    doc shit

---
+++ js-chat.js
@@ -0,0 +1,77 @@
+/**
+ * Person constructor.
+ *
+ * @constructor
+ * @param {String} name The person's name.
+ * @return A new person.
+ */
+function Person(name) {
+  this.name = name;
+}
+
+
+/**
+ * Sends a message to the server.
+ *
+ * @member Person
+ * @param {String} message The message to send to the server.
+ * @returns Person
+ */
+Person.prototype.say = function(message) {
+  // TODO: Replace this with the server POST which stores the message.
+  if (typeof(jQuery) == 'undefined') return;
+  $('#chat-log').append(this.name + ': ' + message + '\n');
+  // $.Ajax({
+  //   type: 'POST',
+  //   url: '/messages',
+  //   data: {'name': name, 'message': message},
+  //   success: function() {},
+  //   error:   function() {},
+  // });
+  return this;
+}
+
+
+/**
+ * Server contructor.
+ *
+ * @constructor
+ * @param {Integer} interval The number of milliseconds in which to poll the server.
+ * @return A server instance.
+ */
+function Server(interval) {
+  // Default interval is 5 seconds (5000ms).
+  this.interval = (typeof(interval) == 'undefined' ? 5000 : interval);
+}
+
+/**
+ * Sets up the Server (timers and misc. other options).
+ *
+ * @member Server
+ * @param {Boolean} initialPoll If true, the server is polled immediately when initializing.
+ * @returns Server instance.
+ */
+Server.prototype.init = function( initialPoll ) {
+  if (initialPoll) this.poll();
+  setInterval(this.poll, this.interval);
+  return this;
+};
+
+/**
+ * This method is called on an interval to poll the server for new messages.
+ *
+ * @member Server
+ * @returns Server instance.
+ */
+Server.prototype.poll = function() {
+  // TODO: Replace this with the code that polls the server.
+  if (typeof(jQuery) == 'undefined') return;
+  $('#chat-log').append('Server polling for new messages...\n');
+  // $.Ajax({
+  //   type: 'GET',
+  //   url: '/json',
+  //   success: function() {},
+  //   error:   function() {},
+  // });
+  return this;
+};
