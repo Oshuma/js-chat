272255f0cb7b1f1aad3a5d317e253b9a0d64eb81
Dale Campbell <dale@save-state.net>
2009-07-03 00:35:02 -0500

    javascript for the demo

--- examples/html/application.js
+++ examples/html/application.js
@@ -5,20 +5,26 @@ function element(id) {
 
 // This function will be called once the DOM is loaded.
 window.onload = function() {
+  // 'Database' to store the messages; with a few test messages.
+  var allMessages = [
+    'Robot 1: Kill all humans.',
+    'Robot 420: Smoke all humans.',
+    'Human: What?'
+  ];
+
   // Create a new server instance.
   var server = new Server();
 
   // Overload the send method to just append the person's name
   // and the message to an element.
   server.send = function(person, message) {
-    element('chat-log').innerHTML += person.name + ': ' + message + '\n';
-    // Store the message.
+    allMessages.push(person.name + ': ' + message);
+    server.poll();
   };
 
   // Overload the poll method to get any new messages from the server.
   server.poll = function() {
-    element('chat-log').innerHTML += 'Polling...\n';
-    // Get some messages and do something...
+    element('chat-log').innerHTML = allMessages.join('\n');
   };
 
   // Now run the server.
