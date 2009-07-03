16c5de95167e42fc97a98fd4490ff04cbb1b7a98
Dale Campbell <dale@save-state.net>
2009-07-03 00:01:51 -0500

    making the sinatra app a little better

--- examples/sinatra/chat-serv.rb
+++ examples/sinatra/chat-serv.rb
@@ -18,6 +18,12 @@ class Message
     "#{name}: #{body}"
   end
 
+  # 'Saves' the message to the 'database' (see Message.all).
+  def save
+    self.class.all << self
+    self
+  end
+
   class << self
     # This is the 'database' that stores all the messages.
     def all
@@ -26,30 +32,31 @@ class Message
   end
 end
 
+# Setup the Sinatra app.
 configure do
+  # Holds javascript, stylesheets, etc.
   set :public, File.dirname(__FILE__) + '/public'
 
+  # Create a few test messages.
   3.times do |i|
-    attrs = { 'name' => "test-#{i}", 'body' => "content for #{i}" }
-    Message.all << Message.new(attrs)
+    attrs = { 'name' => "Robot-#{i}", 'body' => "Prime directive #{rand(i + 1000)}" }
+    Message.new(attrs).save
   end
 end
 
-# Front end.
+# Front end; form and shit.
 get '/' do
   headers 'Content-Type' => 'text/html; charset=utf-8'
   @messages = Message.all
   erb :home
 end
 
-# Returns the messages in JSON format.
+# Returns the messages as a big ass string.
 get '/messages' do
   Message.all.map { |message| message.to_s }.join("\n")
 end
 
-# Creates a new Message
+# Creates a new Message.
 post '/messages' do
-  message = Message.new(params['message'])
-  Message.all << message
-  message
+   Message.new(params['message']).save
 end
